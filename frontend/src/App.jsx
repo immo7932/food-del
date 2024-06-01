f829dd6006c6144e6dfbacfdf05b5fff9a403e5c
MD IMRAN ALAM <97190376+immo7932@users.noreply.github.com>
2024-06-01 16:37:14 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import  Navbar  from './components/Navbar/Navbar.jsx';
+
+const App = () => {
+  return (
+    <>
+      <div className='app'>
+        <Navbar></Navbar>
+      </div>
+    </>
+  );
+};
+export default App
