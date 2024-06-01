61edcb1ed75ea9ee76655b7186183c50396aca41
MD IMRAN ALAM <97190376+immo7932@users.noreply.github.com>
2024-06-01 19:48:29 +0530

    explore-menu

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import  Navbar  from './components/Navbar/Navbar.jsx';
-
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home/Home.jsx'
+import Cart from './pages/Cart/Cart.jsx'
+import PlaceOrder from './pages/PlaceOrder/PlaceOrder.jsx'
 const App = () => {
   return (
     <>
       <div className='app'>
         <Navbar></Navbar>
+        <Routes>
+          <Route path='/' element = {<Home/>}></Route>
+          <Route path='/cart' element = {<Cart/>}/>
+          <Route path='order' element ={<PlaceOrder/>}/>
+        </Routes>
       </div>
     </>
   );
---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Header.css'
+const Header = () => {
+  return (
+    <div className='header'>
+    <div className='header-contents'>
+        <h2>Order your favorite food</h2>
+        <p>order your favorite food from our restaurent for better taste</p>
+        <button>View Menu</button>
+    </div>
+    </div>
+  )
+}
+
+export default Header
