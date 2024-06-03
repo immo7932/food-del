42ec836fde0c6a73c095a5c0f986a44b032373db
MD IMRAN ALAM <97190376+immo7932@users.noreply.github.com>
2024-06-03 23:19:49 +0530

    frontend-completed

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,13 +1,16 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import './Navbar.css';
 import { assets } from '../../assets/assets';
 import { Link } from 'react-router-dom';
+import { StoreContext } from '../../context/StoreContext';
 
  const Navbar = ({setShowLogin}) => {
   const [menu, setMenu] = useState("home");
+  const {getTotalCartAmount} = useContext(StoreContext)
   return (
     <div className='navbar'>
-      <img src={assets.logo} alt="Logo" className="logo" />
+    <Link to={'/'}> <img src={assets.logo} alt="Logo" className="logo" /></Link>
+     
     <ul className="navbar-menu">
       <Link to={'/'} onClick={()=>setMenu("home")} className={menu === "home" ? "active" : ""}>Home</Link>
       <a href='#explore-menu' onClick={()=>setMenu("menu")} className={menu === "menu" ? "active" : ""}>menu</a>
@@ -17,8 +20,9 @@ import { Link } from 'react-router-dom';
     <div className="navbar-right">
        <img src={assets.search_icon}></img>
        <div className="navbar-search-icon">
-        <img src={assets.basket_icon}></img>
-        <div className="dot"></div>
+       <Link to={'/cart'}> <img src={assets.basket_icon}></img></Link>
+       
+        <div className={getTotalCartAmount() === 0 ? "" : "dot"}></div>
        </div>
        <button onClick={()=>setShowLogin(true)}>sign in</button>
     </div>
--- frontend/src/pages/PlaceOrder/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -1,8 +1,57 @@
 import React from 'react'
 import './PlaceOrder.css'
+import { useContext } from 'react'
+import { StoreContext } from '../../context/StoreContext'
 const PlaceOrder = () => {
+
+  const {getTotalCartAmount} = useContext(StoreContext)
   return (
-    <div>PlaceOrder</div>
+    <>
+      <form className='place-order'>
+        <div className='place-order-left'>
+          <p className='title'>Delivery information</p>
+          <div className='multi-fields'>
+            <input type='text' placeholder='First name'></input>
+            <input type='text' placeholder='last name'></input>
+          </div>
+
+          <input type='email' placeholder='enter your email'></input>
+          <input type='text' placeholder='street'></input>
+
+          <div className='multi-fields'>
+            <input type='text' placeholder='city'></input>
+            <input type='text' placeholder='state'></input>
+          </div>
+          <div className='multi-fields'>
+            <input type='number' placeholder='zip code'></input>
+            <input type='text' placeholder='country'></input>
+          </div>
+          <input type='enum' placeholder='phone'></input>
+        </div>
+        <div className='place-order-right'>
+        <div className='cart-total'>
+            <h2>Cart Total</h2>
+            <div>
+            <div className='cart-total-details'>
+                <p>Subtotal</p>
+                <p>${getTotalCartAmount()}</p>
+              </div>
+              <hr></hr>
+              <div className='cart-total-details'>
+                <p>Delivery Fee</p>
+                <p>${getTotalCartAmount() === 0 ? 0 : 2}</p>
+              </div>
+              <hr></hr>
+              <div className='cart-total-details'>
+                <b>Total</b>
+                <b>${getTotalCartAmount() === 0 ? 0 : getTotalCartAmount()+2}</b>
+              </div>
+            </div>
+            <button >PROCEED TO PAYMENT</button>
+          </div>
+        </div>
+      </form>
+    </>
   )
 }
 
