4135deb73613eea2cf6a821caac929e6310cd9c6
MD IMRAN ALAM <97190376+immo7932@users.noreply.github.com>
2024-06-06 20:20:39 +0530

    creating admin panel

---
+++ admin/src/pages/List/List.jsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from 'react'
+import './List.css'
+import axios from 'axios'
+import {toast} from "react-toastify"
+const List = () => {
+const url = "http://localhost:4000/api/food/list"
+  const [list, setList] = useState([])
+
+  const fetchList = async()=>{
+     const response = await axios.get(url);
+     console.log(response.data)
+     if(response.data.success){
+      setList(response.data.data)
+     }
+     else{
+        toast.error("Error")
+     }
+  }
+
+  useEffect(()=>{
+    fetchList();
+  },[])
+  return (
+    <div className='list add flex-col'>
+    <p>All Food List</p>
+    <div className='list-table'>
+      <div className='list-table-format title'>
+      <b>Image</b>
+      <b>Name</b>
+      <b>Category</b>
+      <b>Price</b>
+      <b>Action</b>
+      </div>
+    </div>
+    </div>
+  )
+}
+
+export default List
--- backend/controllers/foodControllers.js
+++ backend/controllers/foodControllers.js
@@ -1,8 +1,8 @@
 import foodModel from "../models/foodModel.js";
-import fs from "fs"
+import fs from "fs";
 
 const addFood = async (req, res) => {
-    let image_filename = req.file.filename
+    let image_filename = req.file.filename;
 
     const food = new foodModel({
         name: req.body.name,
@@ -10,40 +10,38 @@ const addFood = async (req, res) => {
         price: req.body.price,
         category: req.body.category,
         image: image_filename
-    })
-
-
+    });
 
     try {
         await food.save();
-        res.json({ success: true, message: "Food Added" })
+        res.json({ success: true, message: "Food Added" });
     } catch (error) {
-        console.log(error)
-        res.json({ success: false, message: "Error" })
+        console.log(error);
+        res.status(500).json({ success: false, message: "Error adding food" });
     }
-}
+};
 
 const listFood = async (req, res) => {
     try {
         const foods = await foodModel.find({});
-        res.json({ success: true, data: foods })
+       
+        res.json({ success: true, data: foods });
     } catch (error) {
-        console.log(error)
-        res.json({ success: false, message: error })
+        console.log(error);
+        res.status(500).json({ success: false, message: "Error fetching foods" });
     }
-}
-
+};
 
 const removeFood = async (req, res) => {
     try {
         const food = await foodModel.findById(req.body.id);
-        fs.unlink(`uploads/${food.image}`, () => { })
+        fs.unlink(`uploads/${food.image}`, () => { });
         await foodModel.findByIdAndDelete(req.body.id);
-        res.json({success : true, message : "Food removed"});
+        res.json({ success: true, message: "Food removed" });
     } catch (error) {
-            console.log(error);
-            res.json({success : false, message : "Error"})
+        console.log(error);
+        res.status(500).json({ success: false, message: "Error removing food" });
     }
-}
+};
 
-export { addFood, listFood, removeFood }
+export { addFood, listFood, removeFood };
