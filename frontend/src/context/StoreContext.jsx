2f515013fbcde414edb5ec043a14bd4e0e50ea10
MD IMRAN ALAM <97190376+immo7932@users.noreply.github.com>
2024-06-18 13:23:39 +0530

    changes

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -5,7 +5,7 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
-  const url = "https://food-del-backend-2teu.onrender.com";
+  const url = "http://localhost:4000";
   const [token, setToken] = useState("");
   const [food_list, setFood_list] = useState([]);
   const [loading, setLoading] = useState(true);
