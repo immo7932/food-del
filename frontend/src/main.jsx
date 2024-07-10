d3249fe35d2422d0e184bd9ed4d4db1879a50aa5
MD IMRAN ALAM <97190376+immo7932@users.noreply.github.com>
2024-07-10 15:13:04 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,13 +4,10 @@ import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import StoreContextProvider from './context/StoreContext.jsx'
-import 'react-toastify/dist/ReactToastify.css';
-import { ToastContainer } from 'react-toastify';
 ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>
         <StoreContextProvider>
             <App />
-            <ToastContainer position="top-center" />
         </StoreContextProvider>
     </BrowserRouter>
 
